bc816cc2e65fc360122629d04d19e1ebf99e4300
klimrus61 <klimrus61@mail.ru>
2022-11-19 09:15:56 +0300

    апи токен добавлен в сетингс и виев

--- parktronic/parktronic/urls.py
+++ parktronic/parktronic/urls.py
@@ -1,8 +1,11 @@
-from django.urls import include, path
 from django.contrib import admin
+from django.urls import path
+from web_app import views
+from rest_framework.authtoken.views import obtain_auth_token
 
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.Index.as_view()),
+    path('token/', obtain_auth_token),
 ]
